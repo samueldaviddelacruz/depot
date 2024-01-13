4f0028d86ede1d55e5ede4e8762ce295e0e8cf89
Samuel De La Cruz <samuel@codetiger.co>
2024-01-13 08:52:34 -0400

    Add uniqueness validation for title and image URL format validation
    Update product fixtures with new data
    
    Add tests for product validations

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products # load products fixture
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid? # assert product is not valid
+    assert product.errors[:title].any? # assert product has an error on :title
+    assert product.errors[:description].any? # assert product has an error on :description
+    assert product.errors[:price].any? # assert product has an error on :price
+    assert product.errors[:image_url].any? # assert product has an error on :image_url
+  end
+  test "product price must be positive" do
+    product = Product.new(title: "My Book Title", description: "yyy", image_url: "zzz.jpg")
+    product.price = -1 # set price to -1
+    assert product.invalid? # assert product is not valid
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price] # assert product has an error on :price
+    product.price = 0 # set price to 0
+    assert product.invalid? # assert product is not valid
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price] # assert product has an error on :price
+    product.price = 1 # set price to 1
+    assert product.valid? # assert product is valid
+  end
+  def new_product(image_url)
+    Product.new(title: "My Book Title", description: "yyy", price: 1, image_url: image_url)
+  end
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif } # array of valid image urls
+    bad = %w{ fred.doc fred.gif/more fred.gif.more } # array of invalid image urls
+    ok.each do |name| # for each valid image url
+      assert new_product(name).valid?, "#{name} must be valid" # assert product is valid
+    end
+    bad.each do |name| # for each invalid image url
+      assert new_product(name).invalid?, "#{name} must be invalid" # assert product is not valid
+    end
+  end
 end
